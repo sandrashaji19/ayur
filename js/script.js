18f42d72d8ac8f0f1a46041c3d418376c56aa848
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-05-09 00:44:29 +0530

    removed

--- js/script.js
+++
@@ -1,5 +0,0 @@
-function moveToNextPage(parameter,id) {
-    localStorage.setItem('parameter',parameter)
-    // Change the location to the next HTML page
-    window.location.href = "/doctorappo?parameter=" + encodeURIComponent(id);
-}
